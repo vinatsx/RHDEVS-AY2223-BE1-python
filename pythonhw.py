6374ced07352f24d85ff68662fb9f72c67d5b536
kwangyy <chia.kwang.yang@gmail.com>
2022-09-15 21:47:06 +0800

    Update pythonhw.py

--- pythonhw.py
+++ pythonhw.py
@@ -25,47 +25,53 @@ request = {
 
 
 def processRequest(request):
-    applicants = request["applicants"]
+    try:
 
-    def banned(applicant):
-        return True if applicant in bannedVisitors else False
+        applicants = request["applicants"]
 
-    banned_applicants = list(filter(banned, applicants))
-    successful_applicants = [applicant for applicant in applicants if applicant not in banned_applicants]
+        def banned(applicant):
+            return True if applicant in bannedVisitors else False
 
-    def total_sum(applicant_list, member_status):
-        status_list = []
-        price_list = []
-        for applicant in applicant_list:
-            if applicant in memberStatus.keys() and memberStatus[applicant] == True:
-                status_list.append(True)
-                price_list.append(3.5)
-            else:
-                status_list.append(False)
-                price_list.append(5)
-        return status_list, price_list
+        banned_applicants = list(filter(banned, applicants))
+        successful_applicants = [applicant for applicant in applicants if applicant not in banned_applicants]
 
-    status_list, price_list = total_sum(successful_applicants, memberStatus)
+        def total_sum(applicant_list, member_status):
+            status_list = []
+            price_list = []
+            for applicant in applicant_list:
+                if applicant in memberStatus.keys() and memberStatus[applicant] == True:
+                    status_list.append(True)
+                    price_list.append(3.5)
+                else:
+                    status_list.append(False)
+                    price_list.append(5)
+            return status_list, price_list
 
-    def ticketDict(applicant, status, price):
-        ticket_list = []
-        for i in range(len(applicant)):
-            d = {
-                "name": applicant[i],
-                "membershipStatus": status[i],
-                "price": price[i],
-            }
-            ticket_list.append(d)
-        return ticket_list
+        status_list, price_list = total_sum(successful_applicants, memberStatus)
 
+        def ticketDict(applicant, status, price):
+            ticket_list = []
+            for i in range(len(applicant)):
+                d = {
+                    "name": applicant[i],
+                    "membershipStatus": status[i],
+                    "price": price[i],
+                }
+                ticket_list.append(d)
+            return ticket_list
 
-    process = {
-        "successfulApplicants": successful_applicants,
-        "bannedApplicants": banned_applicants,
-        "totalCost": sum(price_list),
-        "tickets": ticketDict(successful_applicants, status_list, price_list)
-    }
-    return process
+        process = {
+            "successfulApplicants": successful_applicants,
+            "bannedApplicants": banned_applicants,
+            "totalCost": sum(price_list),
+            "tickets": ticketDict(successful_applicants, status_list, price_list)
+        }
+        return process
+    
+    except Exception as e:
+        return {
+            "error": "No applicants"
+        }
 
 
 print(processRequest(request))
