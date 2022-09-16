e1352ed3b6a3b62b2cb0161ca9a424bd055de072
Javiery3889 <javiery3889@gmail.com>
2022-09-16 11:08:50 +0800

    Updated pythonhw.py to fit test cases

--- pythonhw.py
+++ pythonhw.py
@@ -23,10 +23,55 @@ request = {
     "applicants": ["Amy", "Ally", "David", "Brendan", "Zoho"]
 }
 
+def filter_banned(applicant):
+    if applicant in bannedVisitors:
+        return True
+    else:
+        return False
+
+def get_membership(filtered_list):
+    membership_dict = {}
+    for applicant in filtered_list:
+        if applicant in memberStatus:
+            if memberStatus.get(applicant) is True:
+                membership_dict[applicant] = "member"
+            else:
+                membership_dict[applicant] = "visitor"
+        else:
+            membership_dict[applicant] = "visitor"
+    return membership_dict
+
+def calc_cost(membership_dict):
+    total_cost = 0
+
+    for status in membership_dict.values():
+        if status == "visitor":
+            total_cost+=5.0
+        else:
+            total_cost+=3.50
+    return total_cost
+
 
 def processRequest(request):
     # Your code here
-    return
+    try:
+        tickets_list = []
+        applicants_list = request.get("applicants")
+        if not applicants_list:
+            raise Exception()
+        banned_list = [x for x in filter(filter_banned,applicants_list)]
+        for banned in banned_list:
+            applicants_list.remove(banned)
+        membership_dict = get_membership(applicants_list)
+        total_cost = calc_cost(membership_dict)
+        for key, value in membership_dict.items():
+            if value == "member":
+                tickets_list.append({"name":key,"membershipStatus":value,"price":3.50})
+            else:
+                tickets_list.append({"name":key,"membershipStatus":value,"price":5})
+        return {"successfulApplicants":applicants_list,"bannedApplicants":banned_list,"totalCost":total_cost,"tickets":tickets_list}
+    except Exception:
+       return {"error": "No applicants"}
 
 
 print(processRequest(request))
