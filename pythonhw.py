c92bdd0a77d31859e01ef8294431d51b21a14d9b
Sheeepen <chengxi1020@gmail.com>
2022-09-22 00:20:57 +0800

    waddup hw

--- pythonhw.py
+++ pythonhw.py
@@ -25,9 +25,58 @@ request = {
 
 
 def processRequest(request):
-    # Your code here
-    return
+    applicant_list = request.get("applicants")
+    if not applicant_list:
+        raise Exception("No applicants lah don't anyhow")
+    
+    banned_applicants = identifyBanned(applicant_list)
+    successful_applicants = identifySuccessful(applicant_list)
+    members = memberStatus.keys()
+    
+    total_cost = 0
+    tickets = []
 
+    for name in applicant_list:
+        if name in members and memberStatus.get(name) == True:
+            total_cost += 3.5
+            tickets.append({
+                "Name": name,
+                "Membership Status": True,
+                "Ticket Price": "$3.50"
+            })
+        else:
+            total_cost += 5
+            tickets.append({
+                "Name": name,
+                "Membership Status": False,
+                "Ticket Price": "$5.00"
+            })
+    display = {
+        "Banned Applicants": banned_applicants,
+        "Total Price": total_cost,
+        "Tickets": tickets
+    }
+
+    return display
+
+
+        
+def identifyBanned(applicants):
+    banned = []
+    for name in applicants:
+        if name in bannedVisitors:
+            banned.append(name)
+    return banned
+
+def identifySuccessful(applicants):
+    successful = []
+    for name in applicants:
+        if name not in bannedVisitors:
+            successful.append(name)
+    return successful
+
+
+        
 
 print(processRequest(request))
 
